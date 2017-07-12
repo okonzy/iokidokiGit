b9b8330020795d80785d3f602e813537014e77b3
Christopher Ok'Onkwo <chris@iokmcbookpro.home>
2017-07-12 01:17:01 +0100

    Initial Commit

---
+++ First/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First
+//
+//  Created by Christopher Ok'Onkwo on 12/07/2017.
+//  Copyright © 2017 Christopher Ok'Onkwo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Christopher Ok'Onkwo on 12/07/2017.
+//  Copyright © 2017 Christopher Ok'Onkwo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
